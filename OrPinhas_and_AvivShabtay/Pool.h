c8dba467a067ec33a77540166a91b55ff4797d48
Aviv Shbaty <shabtay.aviv@gmail.com>
2021-06-05 15:27:52 +0300

    Update pool

--- OrPinhas_and_AvivShabtay/Pool.h
+++ OrPinhas_and_AvivShabtay/Pool.h
@@ -16,14 +16,36 @@ public:
 	 * Not doing anything, just create empty prototype of pool.
 	 */
 	Pool() = default;
+
 	/*
 	 * Define new Pool in the hotel.
 	 */
 	explicit Pool(bool isHavingEntertainment, const char* hours);
+
 	/*
-	 * Release all the allocated memory of the pool
+	 * Return is the pool have entertainment.
 	 */
-	~Pool();
+	bool doesHavingEntertainment() const;
+
+	/*
+	 * Set entertainment status for this pool.
+	 */
+	void setIsHavingEntertainment(bool isHavingEntertainment);
+
+	/*
+	 * Return string represent the opening hour fot this pool.
+	 */
+	const char* getOpeningHours() const;
+
+	/*
+	 * Set the opening hours for this pool.
+	 */
+	void setOpeningHours(const char* openingHours);
+
+	/*
+	 * Print the pool details.
+	 */
+	friend ostream& operator<<(ostream& os, const Pool& pool);
 };
 #endif
 
